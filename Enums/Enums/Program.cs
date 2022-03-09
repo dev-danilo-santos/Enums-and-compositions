95a0ecad8e18cc67fc6fbc7b25a7f3f4d1ff9a1c
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-03-09 00:00:22 -0300

    Iniciando a primeira atividade relacionada ao módulo

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public int x;  // advertência apenas de por não usar a variável
+            
+        }
+    }
+}  
+
