bea1e37f6126238c9899479752ac949a421c81b1
Danilo Santos <danilo99ambrosio@hotmail.com>
2022-03-12 21:00:47 -0300

    Feito a atividade 2 referente a aula de stringBuilder

---
+++ ActivityPost/ActivityPost/Entities/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityPost.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void addComent(Comment c) 
+        {
+            Comments.Add(c);
+        }
+
+        public void removecomment(Comment c)
+        {
+            Comments.Remove(c); 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.AppendLine("Likes "+Likes + " - " + Moment);
+            sb.AppendLine(Content);
+            sb.AppendLine("comments:");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.text);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ ActivityPost/ActivityPost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ActivityPost.Entities;
+
+namespace ActivityPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Post post1 = new(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                46
+                );
+            post1.addComent(new Comment("Have a nice trip"));
+            post1.addComent(new Comment("Wow that's awesome!"));
+
+            Post post2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+               );
+            post2.addComent(new Comment("Good night"));
+            post2.addComent(new Comment("May the force be with you"));
+
+
+            Console.WriteLine(post1);
+            Console.WriteLine(post2);
+        }
+    }
+}
